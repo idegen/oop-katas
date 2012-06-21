0cae9325cacab6edbc95a16fc868e43bf143c09e
idegen <i_degen@hotmail.com>
2012-06-21 10:42:36 +0100

    ID: fixed the extended Address test

--- basic-code-smells/test/extended/AddressTest.java
+++ basic-code-smells/test/extended/AddressTest.java
@@ -1,7 +1,5 @@
 package extended;
 
-import initial.Address;
-import initial.Employee;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -10,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class AddressTest {
     @Test
     public void testPrintsAddress() throws Exception {
-        initial.Employee employee = new Employee("Felicity", "Brooks", 5, "", "Lemon Road", "Cantebury", "W5 C56");
+        Employee employee = new Employee("Felicity", "Brooks", 5, "", "Lemon Road", "Cantebury", "W5 C56");
         Address address = new Address(employee);
 
         assertThat(address.printAddress(), is("5; ; Lemon Road; Cantebury; W5 C56"));
